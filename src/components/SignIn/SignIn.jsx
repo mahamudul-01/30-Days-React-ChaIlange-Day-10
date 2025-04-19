318f96004e376d98940851927f16bb5223372a8e
mahamudul-01 <mahamudul.hasan.mehedi.01@gmail.com>
2025-04-19 21:11:55 +0600

    add email sign-in and sign-up functionality with form handling in SignIn and SignUp components

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -6,12 +6,22 @@ import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 
 const SignIn = () => {
+
+
   const navigate = useNavigate();
 
   const [user, setUser] = useState(null);
 
   const googleProvider = new GoogleAuthProvider();
   const githubProvider = new GithubAuthProvider();
+
+  const handleEmailSignIn = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+
+  }
 
   const handleGoogleSignIn = () => {
     // Handle Google sign-in logic here
@@ -50,20 +60,20 @@ const SignIn = () => {
       });
   };
   return (
-    <div class="flex flex-col max-w-md p-6 rounded-md sm:p-10 dark:bg-gray-50 dark:text-gray-800 mx-auto">
-      <div class="mb-8 text-center">
-        <div className="flex justify-center items-center">
-          <h1 class="my-3  text-4xl font-bold">Sign in</h1>
+    <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 dark:bg-gray-50 dark:text-gray-800 mx-auto">
+      <div className="mb-8 text-center">
+        <div classNameName="flex justify-center items-center">
+          <h1 className="my-3  text-4xl font-bold">Sign in</h1>
           {user && (
-            <img className="rounded-full w-20" src={user?.photoURL} alt="" />
+            <img classNameName="rounded-full w-20" src={user?.photoURL} alt="" />
           )}
         </div>
-        <p class="text-sm dark:text-gray-600">Sign in to access your account</p>
+        <p className="text-sm dark:text-gray-600">Sign in to access your account</p>
       </div>
-      <form noValidate="" action="" class="space-y-12">
-        <div class="space-y-4">
+      <form onSubmit={handleEmailSignIn} noValidate="" action="" className="space-y-12">
+        <div className="space-y-4">
           <div>
-            <label htmlFor="email" class="block mb-2 text-sm">
+            <label htmlFor="email" className="block mb-2 text-sm">
               Email address
             </label>
             <input
@@ -71,18 +81,18 @@ const SignIn = () => {
               name="email"
               id="email"
               placeholder="leroy@jenkins.com"
-              class="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
+              className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
             />
           </div>
           <div>
-            <div class="flex justify-between mb-2">
-              <label htmlFor="password" class="text-sm">
+            <div className="flex justify-between mb-2">
+              <label htmlFor="password" className="text-sm">
                 Password
               </label>
               <a
                 rel="noopener noreferrer"
                 href="#"
-                class="text-xs hover:underline dark:text-gray-600"
+                className="text-xs hover:underline dark:text-gray-600"
               >
                 Forgot password?
               </a>
@@ -92,15 +102,15 @@ const SignIn = () => {
               name="password"
               id="password"
               placeholder="*****"
-              class="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
+              className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
             />
           </div>
         </div>
-        <div class="space-y-2">
+        <div className="space-y-2">
           <div>
             <button
-              type="button"
-              class="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50"
+              type="submit"
+              className="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50"
             >
               Sign in
             </button>
@@ -108,7 +118,7 @@ const SignIn = () => {
               <button
                 onClick={handleSignOut}
                 type="button"
-                class="w-full mt-6 px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50"
+                className="w-full mt-6 px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50"
               >
                 Sign out
               </button>
@@ -116,27 +126,27 @@ const SignIn = () => {
             <button
               onClick={handleGoogleSignIn}
               type="button"
-              class="w-full mt-6 px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50"
+              className="w-full mt-6 px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50"
             >
               Sign in with google
-              <FcGoogle  className="inline ml-4" />
+              <FcGoogle  classNameName="inline ml-4" />
             </button>
             <button
               onClick={handleGithubSignIn}
               type="button"
-              class="w-full mt-6 px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50"
+              className="w-full mt-6 px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50"
             >
               Sign in with github
-              <FaGithub  className="inline ml-4" />
+              <FaGithub  classNameName="inline ml-4" />
             </button>
           </div>
-          <p class="px-6 text-sm text-center dark:text-gray-600">
+          <p className="px-6 text-sm text-center dark:text-gray-600">
             Don't have an account yet?
             <Link
               to="/sign-up"
               rel="noopener noreferrer"
               href="#"
-              class="hover:underline dark:text-violet-600"
+              className="hover:underline dark:text-violet-600"
             >
               Sign up
             </Link>
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,16 +1,24 @@
 import { FaGoogle } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 const SignUp = () => {
+  const handleEmailSignUp = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(name, email, password);
+  }
+
   return (
-    <div class="flex flex-col max-w-md p-6 rounded-md sm:p-10 dark:bg-gray-50 dark:text-gray-800 mx-auto">
-      <div class="mb-8 text-center">
-        <h1 class="my-3 text-4xl font-bold">Sign Up</h1>
-        <p class="text-sm dark:text-gray-600">Sign up to access your account</p>
+    <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 dark:bg-gray-50 dark:text-gray-800 mx-auto">
+      <div className="mb-8 text-center">
+        <h1 className="my-3 text-4xl font-bold">Sign Up</h1>
+        <p className="text-sm dark:text-gray-600">Sign up to access your account</p>
       </div>
-      <form noValidate="" action="" class="space-y-12">
-        <div class="space-y-4">
+      <form noValidate="" onSubmit={handleEmailSignUp} action="" className="space-y-12">
+        <div className="space-y-4">
           <div>
-            <label htmlFor="email" class="block mb-2 text-sm">
+            <label htmlFor="email" className="block mb-2 text-sm">
               Full Name
             </label>
             <input
@@ -18,11 +26,11 @@ const SignUp = () => {
               name="name"
               id="name"
               placeholder="leroy@jenkins.com"
-              class="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
+              className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
             />
           </div>
           <div>
-            <label htmlFor="email" class="block mb-2 text-sm">
+            <label htmlFor="email" className="block mb-2 text-sm">
               Email address
             </label>
             <input
@@ -30,18 +38,18 @@ const SignUp = () => {
               name="email"
               id="email"
               placeholder="leroy@jenkins.com"
-              class="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
+              className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
             />
           </div>
           <div>
-            <div class="flex justify-between mb-2">
-              <label htmlFor="password" class="text-sm">
+            <div className="flex justify-between mb-2">
+              <label htmlFor="password" className="text-sm">
                 Password
               </label>
               <a
                 rel="noopener noreferrer"
                 href="#"
-                class="text-xs hover:underline dark:text-gray-600"
+                className="text-xs hover:underline dark:text-gray-600"
               >
                 Forgot password?
               </a>
@@ -51,34 +59,34 @@ const SignUp = () => {
               name="password"
               id="password"
               placeholder="*****"
-              class="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
+              className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
             />
           </div>
         </div>
-        <div class="space-y-2">
+        <div className="space-y-2">
           <div>
             <button
-              type="button"
-              class="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50"
+              type="submit"
+              className="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50"
             >
               Sign Up
             </button>
             <button
               type="button"
-              class="w-full mt-6 px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50"
+              className="w-full mt-6 px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50"
             >
               Sign Up with google
-              <FaGoogle className="inline ml-4" />
+              <FaGoogle classNameName="inline ml-4" />
             </button>
           </div>
-          <p class="px-6 text-sm text-center dark:text-gray-600">
+          <p className="px-6 text-sm text-center dark:text-gray-600">
             Don't have an account yet?
             <Link
               q
               to="/sign-in"
               rel="noopener noreferrer"
               href="#"
-              class="hover:underline dark:text-violet-600"
+              className="hover:underline dark:text-violet-600"
             >
               Sign in
             </Link>
